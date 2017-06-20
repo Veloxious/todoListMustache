486cde501d91b88782b9fd8cf8874c44d7df21fa
Thomas Gonzales <thomas.j.gonzales@gmail.com>
2017-06-20 12:33:12 -0500

    basic Node checklist

---
+++ mustache-exercise/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const mustacheExpress = require('mustache-express');
+
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+//Listening on root
+app.get('/todo/', function (req, res) {
+  // TODO write your code here
+  let list = []
+  list.push ({todo: 'Node Basics'}, {todo: 'Express Basics'}, {todo: 'Mustache'}, {todo: 'HTML forms with express'}, {todo: 'about authentication'}, {todo: 'how to connect to PostgreSQL'}, {todo: 'how to create databases'}, {todo: 'SQL'}, {todo: 'how to connect to PostgreSQL from Node'}, {todo: 'how to use Sequelize'})
+  res.render('todo', {list :list})
+})
+
+app.listen(3000, function () {
+  console.log('Successfully started express application!');
+})
